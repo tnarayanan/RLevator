4f117d586110aba36fe659553b298e720f15ef9d
Tejas Narayanan <tejasn100@gmail.com>
2022-12-06 04:43:49 -0800

    Add standard elevator controller to test file

--- test_elevator_v1_agent.py
+++ test_elevator_v1_agent.py
@@ -1,7 +1,8 @@
 from envs.elevator_v1 import ElevatorV1Env
+from agents.standard_elevator_controller import StandardElevatorController
 from stable_baselines3 import A2C, PPO
 
-env = ElevatorV1Env(num_elevators=1, num_floors=3, episode_len=300)
+env = ElevatorV1Env(num_elevators=3, num_floors=10, episode_len=300)
 
 # 1 elev 3 floors, ep len 300, 150k, +100 rew on success
 # ppo_mlp/PPO_2
@@ -13,11 +14,12 @@ env = ElevatorV1Env(num_elevators=1, num_floors=3, episode_len=300)
 
 
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./tensorboard/ppo_mlp/")
-model.learn(total_timesteps=150_000)
+model.learn(total_timesteps=1_000_000)
+# model = StandardElevatorController(env)
 
 obs = env.reset()
 total_reward = 0
-for i in range(1000):
+for i in range(2000):
     action, _ = model.predict(obs, deterministic=True)
     obs, reward, done, _ = env.step(action)
     total_reward += reward
