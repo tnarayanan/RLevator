7ddb0a28e4c89371b6f222effc1a7e1bfba2d359
Tejas Narayanan <tejasn100@gmail.com>
2022-12-08 23:30:32 -0800

    Elevator v3 with observation space changes

---
+++ agents/standard_elevator_v3_controller.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+class StandardElevatorV3Controller:
+    def __init__(self, env):
+        self.env = env
+
+    def predict(self, obs: np.array, deterministic=True):
+        obs = obs.astype(np.int32)
+        elevators = [{} for _ in range(self.env.num_elevators)]
+        i = 2
+        for elev in elevators:
+            elev['floor'] = obs[i]
+            elev['target'] = obs[i+1]
+            elev['time_to_next_floor'] = obs[i+2]
+            elev['direction'] = obs[i+3]
+            i += 4
+            floor_requests = []
+            for k in range(self.env.num_floors):
+                floor_requests.append(obs[i+k])
+            i += self.env.num_floors_end
+            elev['requests'] = floor_requests
+
+        up_pressed = []
+        down_pressed = []
+        for k in range(self.env.num_floors):
+            up_pressed.append(obs[i+k])
+        i += self.env.num_floors_end
+        for k in range(self.env.num_floors):
+            down_pressed.append(obs[i+k])
+
+        # actual algo
+
+        action = np.zeros(self.env.num_elevators_end)
+
+        for elev_id, elev in enumerate(elevators):
+            closest_stop_below = None
+            for i in reversed(range(0, elev['floor'])):
+                if elev['requests'][i] == 1 or down_pressed[i] == 1:  # if person in elevator wants to get off at `i` or down is pressed on `i`
+                    closest_stop_below = i
+                    break
+
+            closest_stop_above = None
+            for i in range(elev['floor'] + 1, self.env.num_floors):
+                if elev['requests'][i] == 1 or up_pressed[i] == 1:  # if person in elevator wants to get off at `i` or up is pressed on `i`
+                    closest_stop_above = i
+                    break
+
+            if elev['direction'] == 0 and closest_stop_below is not None:
+                action[elev_id] = closest_stop_below
+            elif elev['direction'] == 2 and closest_stop_above is not None:
+                action[elev_id] = closest_stop_above
+            elif closest_stop_below is not None:
+                action[elev_id] = closest_stop_below
+            else:
+                action[elev_id] = elev['floor'] if closest_stop_above is None else closest_stop_above
+
+        return action, ""
--- benchmark_agents_v3.py
+++ benchmark_agents_v3.py
@@ -1,12 +1,12 @@
-from agents.standard_elevator_v2_controller import StandardElevatorV2Controller
-from envs.elevator_v2 import ElevatorV2Env
+from agents.standard_elevator_v3_controller import StandardElevatorV3Controller
+from envs.elevator_v3 import ElevatorV3Env
 from stable_baselines3 import A2C, PPO
 from tqdm import tqdm
 from multiprocessing import Process, Pool
 
 
 def benchmark_agent(model_filepath, num_episodes=100, num_elevators_start=1, num_elevators_end=1, num_floors_start=3, num_floors_end=3):
-    env = ElevatorV2Env(curriculum=True,
+    env = ElevatorV3Env(curriculum=True,
                         num_elevators_start=num_elevators_start,
                         num_elevators_end=num_elevators_end,
                         num_floors_start=num_floors_start,
@@ -14,25 +14,28 @@ def benchmark_agent(model_filepath, num_episodes=100, num_elevators_start=1, num
                         episode_len=300)
 
     if model_filepath == "Standard Controller":
-        model = StandardElevatorV2Controller(env)
+        model = StandardElevatorV3Controller(env)
     else:
         model = PPO.load(model_filepath, env=env)
 
     print(f"Benchmarking {model_filepath}")
 
-    requests_sum = 0
-    dropped_sum = 0
+    # requests_sum = 0
+    # dropped_sum = 0
+    reward_sum = 0
     for _ in tqdm(range(num_episodes)):
         obs = env.reset(override_curriculum=True)
         done = False
         while not done:
             action, _ = model.predict(obs, deterministic=True)
             obs, reward, done, _ = env.step(action)
+            reward_sum += reward
 
-        requests_sum += env.num_total_requests
-        dropped_sum += env.num_dropped_off
+        # requests_sum += env.num_total_requests
+        # dropped_sum += env.num_dropped_off
 
-    print(f"{model_filepath}: {dropped_sum / requests_sum: .3%}")
+    # print(f"{model_filepath}: {dropped_sum / requests_sum: .3%}")
+    print(f"{model_filepath}: {reward_sum / num_episodes}")
 
 
 def main():
@@ -47,10 +50,10 @@ def main():
 
     if num_floors == 5:
         models = [
-            "models/env_v2/elev1-1_floor5-5/55d5b7.zip", # GOOD: non-curriculum ~55% on 5 floors
+            # "models/env_v2/elev1-1_floor5-5/55d5b7.zip", # GOOD: non-curriculum ~55% on 5 floors
             # "models/env_v2/elev1-1_floor5-5/4a3039.zip",
             # "models/env_v2/elev1-1_floor5-5/237766.zip",
-            "models/env_v2/elev1-1_floor3-5/80f213.zip", # GOOD: curriculum ~73% on 5 floors
+            # "models/env_v2/elev1-1_floor3-5/80f213.zip", # GOOD: curriculum ~73% on 5 floors
             # "models/env_v2/elev1-1_floor3-5/e6e1f1.zip",
             # "models/env_v2/elev1-1_floor3-5/ee34c4.zip",
             "Standard Controller"
